21cc50158c9fde640c5ebe1cc3e58d52dad2f8b3
wakassodev <wakasso94@gmail.com>
2024-01-27 13:56:06 +0400

    removing the function name

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,14 +2,16 @@
 const myObject = {
   type: 'object',
   value: 12
-}
-console.log(myObject)
+};
+console.log(myObject);
+
 myObject.incr = function () {
-  myObject.value += 1
-}
-myObject.incr()
-console.log(myObject)
-myObject.incr()
-console.log(myObject)
-myObject.incr()
-console.log(myObject)
+  myObject.value += 1;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
